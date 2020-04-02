afc6c581a2b5f4806e928c1562bc1594d003072f
dewall <a.dewall@52north.org>
2020-04-02 14:10:28 +0200

    pypi preperations

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-requirements = parse_requirements('requirements.txt', session='hack')
+# loading requirements
+requirements = list(parse_requirements('requirements.txt', session='hack'))
+requirements = [r.name for r in requirements]
 
 setup(
     name="enpyrocar",
-    version="0.1",
     packages=find_packages(exclude=["tests", "tests.*"]),
-#    install_requires=requirements,
-    test_suite="tests"
+    version="0.1",
+    license="MIT",
+    url="https://github.com/enviroCar/enpyrocar",
+    keywords=["enviroCar", "trajectory"],
+    install_requires=requirements,
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
