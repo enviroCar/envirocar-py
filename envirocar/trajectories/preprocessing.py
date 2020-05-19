98346b387b7d64bd8fa0daafa126a7daef305507
Anna Formaniuk <a_form03@uni-muenster.de>
2020-05-19 22:05:00 +0200

    consistent naming

--- envirocar/trajectories/preprocessing.py
+++ envirocar/trajectories/preprocessing.py
@@ -4,18 +4,18 @@ import numpy as np
 import datetime
 
 
-class Processing():
+class Preprocessing():
     def __init__(self):
-       print("Hello from basic processing!") # do we need anything here?
+       print("Hello from preprocessing!") # do we need anything here?
 
     def interpolate(self, points):
         """ Creates a trajectory from point data
         
         Keyword Arguments:
-            GeoDataFrame {?} -- A GeoDataFrame containing the track points
+            points {GeoDataFrame} -- A GeoDataFrame containing the track points
         
         Returns:
-            Something -- An interpolated trajectory, hopefully
+            new_points -- An interpolated trajectory, hopefully
         """
 
         # to have flat attributes
@@ -24,16 +24,16 @@ class Processing():
         points_df = pd.DataFrame(points)
 
         # removing duplicates because interpolation won't work otherwise
-        points_df_cleaned = points_df.drop_duplicates(['lat','lng'], keep='last')
+        points_df_cleaned = points_df.drop_duplicates(['lat', 'lng'], keep='last')
 
         # constructing input arrays
-        input_coords_x = np.array(points_df_cleaned.lat.values.tolist())
-        input_coords_y = np.array(points_df_cleaned.lng.values.tolist())
+        input_coords_y = np.array(points_df_cleaned.lat.values.tolist())
+        input_coords_x = np.array(points_df_cleaned.lng.values.tolist())
 
         # Find the B-spline representation of the curve
         # tck (t,c,k): is a tuple containing the vector of knots, the B-spline coefficients, and the degree of the spline.
         # u: is an array of the values of the parameter.
-        tck, u = interpolate.splprep([input_coords_y, input_coords_x], s = 0)
+        tck, u = interpolate.splprep([input_coords_x, input_coords_y], s = 0)
         new_points = interpolate.splev(np.linspace(0, 1, 200), tck) # interpolating 200 points
 
         # TODO interpolate by timestep 
