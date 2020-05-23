10fdfa5aa201d8a383cdb100329401777c1267f6
Saad Saif <msaadsaif5@gmail.com>
2020-05-23 17:58:13 +0200

    add trajectory class, represent track as trajectory for further analysis

--- envirocar/__init__.py
+++ envirocar/__init__.py
@@ -3,3 +3,4 @@ from .client.download_client import DownloadClient
 from .client.api.track_api import TrackAPI
 from .client.request_param import BboxSelector, TimeSelector
 from .trajectories.preprocessing import Preprocessing
+from .trajectories.trajectory import Trajectory
