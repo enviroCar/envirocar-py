35c3eccf974fcca6f53a2b85d25b7bddfe1d7106
sbastiangarzon <js.garzon389@uniandes.edu.co>
2020-06-23 22:53:20 +0200

    Similarity measures implemented with movingpandas trajectories

--- envirocar/__init__.py
+++ envirocar/__init__.py
@@ -5,4 +5,4 @@ from .client.request_param import BboxSelector, TimeSelector
 from .trajectories.preprocessing import Preprocessing
 from .trajectories.preprocessing import GeneralizationType
 from .trajectories.track_converter import TrackConverter
-from .trajectories.track_similarity import TrackSimilarity
+from .trajectories.track_similarity import *
