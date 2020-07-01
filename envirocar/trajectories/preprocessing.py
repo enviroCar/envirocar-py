60bdc98f38164154239011c0142fba208521e4a0
Anna Formaniuk <a_form03@uni-muenster.de>
2020-07-02 00:29:22 +0200

    interpolating multiple tracks at once

--- envirocar/trajectories/preprocessing.py
+++ envirocar/trajectories/preprocessing.py
@@ -198,118 +198,124 @@ class Preprocessing():
         points['lng'] = points['geometry'].apply(lambda coord: coord.x)
         points_df = pd.DataFrame(points)
 
-        # removing duplicates because interpolation won't work otherwise
-        points_df_cleaned = points_df.drop_duplicates(
-            ['lat', 'lng'], keep='last')
+        tracks_dict = dict(iter(points_df.groupby('track.id')))
+        interpolated = []
 
-        # input for datetime in seconds
-        points_df_cleaned['time_seconds'] = np.vectorize(date_to_seconds)(
-            np.array(points_df_cleaned.time.values.tolist()))
+        for track_id in tracks_dict:
+            # removing duplicates because interpolation won't work otherwise
+            points_df_cleaned = tracks_dict[track_id].drop_duplicates(
+                ['lat', 'lng'], keep='last')
 
-        # creating the column name lists
-        names_interpolate = [s for s in points_df_cleaned.columns if
-                             '.value' in s]
-        # adding the other column names at front
-        names_interpolate = ['lng', 'lat', 'time_seconds'] + names_interpolate
-        names_replicatate = np.setdiff1d(points_df_cleaned.columns,
-                                         names_interpolate)
-        names_extra = ['geometry', 'id', 'time']
-        names_replicatate = [x for x in names_replicatate if x
-                             not in names_extra]
+            # input for datetime in seconds
+            points_df_cleaned['time_seconds'] = np.vectorize(date_to_seconds)(
+                np.array(points_df_cleaned.time.values.tolist()))
 
-        time_seconds_array = points_df_cleaned[
-                 'time_seconds'].to_numpy()
+            # creating the column name lists
+            names_interpolate = [s for s in points_df_cleaned.columns if
+                                 '.value' in s]
+            # adding the other column names at front
+            names_interpolate = ['lng', 'lat', 'time_seconds'] + \
+                names_interpolate
+            names_replicatate = np.setdiff1d(points_df_cleaned.columns,
+                                             names_interpolate)
+            names_extra = ['geometry', 'id', 'time']
+            names_replicatate = [x for x in names_replicatate if x
+                                 not in names_extra]
 
-        passed_time = [(time_seconds_array[i+1]-time_seconds_array[i]) for i
-                       in range(len(time_seconds_array)-1)]
-        passed_time = np.insert(passed_time, 0, 0, axis=0)
-        # to interpolate for every meter
-        dist = (points_df_cleaned['Speed.value']/3.6 * passed_time)/1
-        dist_between = [sum(dist[:i+1]) for i in range(len(dist))]
-        dist_between = list(map(int, dist_between))
-        # print(dist_between)
+            time_seconds_array = points_df_cleaned[
+                    'time_seconds'].to_numpy()
 
-        points_df_cleaned['dist_between'] = dist_between
+            passed_time = [(time_seconds_array[i+1]-time_seconds_array[i])
+                           for i in range(len(time_seconds_array)-1)]
+            passed_time = np.insert(passed_time, 0, 0, axis=0)
+            # to interpolate for every meter
+            dist = (points_df_cleaned['Speed.value']/3.6 * passed_time)/10
+            dist_between = [sum(dist[:i+1]) for i in range(len(dist))]
+            dist_between = list(map(int, dist_between))
+            # print(dist_between)
 
-        points_df_cleaned.drop_duplicates(
-            ['dist_between'], keep='first', inplace=True)
+            points_df_cleaned['dist_between'] = dist_between
 
-        dist_between = np.array(
-            points_df_cleaned['dist_between'].values.tolist())
-        # print(dist_between)
+            points_df_cleaned.drop_duplicates(
+                ['dist_between'], keep='first', inplace=True)
 
-        del points_df_cleaned['dist_between']
-        # measurements themselves
-        columns_interpolate = [np.array(
-            points_df_cleaned[column].values.tolist()) for column
-            in names_interpolate]
+            dist_between = np.array(
+                points_df_cleaned['dist_between'].values.tolist())
+            # print(dist_between)
 
-        # split dataframe because splprep cannot take more than 11
-        dfs = np.split(columns_interpolate, [2], axis=0)
+            del points_df_cleaned['dist_between']
+            # measurements themselves
+            columns_interpolate = [np.array(
+                points_df_cleaned[column].values.tolist()) for column
+                in names_interpolate]
 
-        """ Interpolation itself """
-        # Find the B-spline representation of the curve
-        # tck (t,c,k): is a tuple containing the vector of knots,
-        # the B-spline coefficients, and the degree of the spline.
-        # u: is an array of the values of the parameter.
+            # split dataframe because splprep cannot take more than 11
+            dfs = np.split(columns_interpolate, [2], axis=0)
 
-        # interpolating so many points to have a point for every 10 meters
-        # step = np.linspace(0, 1, dist_between[-1] -
-        #                    dist_between[0])
+            """ Interpolation itself """
+            # Find the B-spline representation of the curve
+            # tck (t,c,k): is a tuple containing the vector of knots,
+            # the B-spline coefficients, and the degree of the spline.
+            # u: is an array of the values of the parameter.
 
-        meters = np.linspace(dist_between[0],
-                             dist_between[-1],
-                             dist_between[-1] -
-                             dist_between[0],
-                             dtype='int32')
+            # interpolating so many points to have a point for every 10 meters
+            # step = np.linspace(0, 1, dist_between[-1] -
+            #                    dist_between[0])
 
-        new_points = interpolate_spline(dist_between, dfs[0], meters)
+            meters = np.linspace(dist_between[0],
+                                 dist_between[-1],
+                                 dist_between[-1] -
+                                 dist_between[0],
+                                 dtype='int32')
 
-        for idx, column in enumerate(dfs[1]):
-            new_points.append(interpolate_linear(dist_between, column,
-                                                 meters))
+            new_points = interpolate_spline(dist_between, dfs[0], meters)
 
-        # transposing the resulting matrix to fit it in the dataframe
-        data = np.transpose(new_points)
+            for idx, column in enumerate(dfs[1]):
+                new_points.append(interpolate_linear(dist_between, column,
+                                                     meters))
 
-        # constructing the new dataframe
-        interpolated_df = pd.DataFrame(data)
+            # transposing the resulting matrix to fit it in the dataframe
+            data = np.transpose(new_points)
 
-        print(len(names_interpolate))
+            # constructing the new dataframe
+            interpolated_df = pd.DataFrame(data)
 
-        interpolated_df.columns = names_interpolate
-        interpolated_df['time'] = np.vectorize(
-            seconds_to_date)(interpolated_df['time_seconds'])
+            interpolated_df.columns = names_interpolate
+            interpolated_df['time'] = np.vectorize(
+                seconds_to_date)(interpolated_df['time_seconds'])
 
-        # these should all be the same for one ride, so just replicating
-        columns_replicate = [np.repeat(points_df_cleaned[column].iloc[0],
-                             len(meters)) for column in names_replicatate]
+            # these should all be the same for one ride, so just replicating
+            columns_replicate = [np.repeat(points_df_cleaned[column].iloc[0],
+                                 len(meters)) for column in names_replicatate]
 
-        replicated_transposed = np.transpose(columns_replicate)
-        replicated_df = pd.DataFrame(replicated_transposed)
-        replicated_df.columns = names_replicatate
+            replicated_transposed = np.transpose(columns_replicate)
+            replicated_df = pd.DataFrame(replicated_transposed)
+            replicated_df.columns = names_replicatate
 
-        # combining replicated with interpolated
-        full_df = pd.concat([interpolated_df, replicated_df], axis=1,
-                            sort=False)
+            # combining replicated with interpolated
+            full_df = pd.concat([interpolated_df, replicated_df], axis=1,
+                                sort=False)
 
-        # adding ids
-        full_df['id'] = 0
-        for row in full_df.index:
-            full_df['id'][row] = randStr()
+            # adding ids
+            full_df['id'] = 0
+            for row in full_df.index:
+                full_df['id'][row] = randStr()
 
-        # transforming back to a geodataframe
-        full_gdf = gpd.GeoDataFrame(
-         full_df, geometry=gpd.points_from_xy(full_df.lng, full_df.lat))
+            # transforming back to a geodataframe
+            full_gdf = gpd.GeoDataFrame(
+                full_df, geometry=gpd.points_from_xy(full_df.lng, full_df.lat))
 
-        # remove full_gdf['lng'], full_gdf['lat'] ?
-        del full_gdf['time_seconds']
+            # remove full_gdf['lng'], full_gdf['lat'] ?
+            del full_gdf['time_seconds']
 
-        print('Amount of points after interpolation',
-              full_gdf.shape)
-        # print(full_gdf['track.length'])
+            print('Amount of points after interpolation',
+                  full_gdf.shape)
+            # print(full_gdf['track.length'])
+            interpolated.append(full_gdf)
 
-        return full_gdf
+            combined_again = pd.concat(interpolated)
+
+        return combined_again
 
     def aggregate(self, track_df, MIN_LENGTH, MIN_GAP, MAX_DISTANCE,
                   MIN_DISTANCE, MIN_STOP_DURATION):
